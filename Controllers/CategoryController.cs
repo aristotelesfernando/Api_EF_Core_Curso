9d346ce874ed67f56bad4d0616a3416637ab45ff
A.Fernando Jr <aristotelesfernando.jr@gmail.com>
2020-09-19 21:33:18 -0300

    Controller de Produtos

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Api_Shop.Data;
 using Api_Shop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api_Shop.Controllers
 {
@@ -11,16 +12,24 @@ namespace Api_Shop.Controllers
     {
         [HttpGet]
         [Route("")]
-        public async Task<ActionResult<List<Category>>> Get()
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext db)
         {
-            return new List<Category>();
+            var categories = await db.Categories.AsNoTracking().ToListAsync();
+            return Ok(categories);
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> GetById(int id)
+        public async Task<ActionResult<Category>> GetById(
+            int id,
+            [FromServices] DataContext db)
         {
-            return new Category();
+            var category = await db
+                            .Categories
+                            .AsNoTracking()
+                            .FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(category);
         }
 
         [HttpPost]
@@ -32,15 +41,24 @@ namespace Api_Shop.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            db.Categories.Add(model);
-            await db.SaveChangesAsync();
-
-            return model;
+            try
+            {
+                db.Categories.Add(model);
+                await db.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(new { message = $"ERRO! Não foi possível criar categoria: {ex.Message}" });
+            }
         }
 
         [HttpPut]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
+        public async Task<ActionResult<Category>> Put(
+            int id,
+            [FromServices] DataContext db,
+            [FromBody] Category model)
         {
             if (id != model.Id)
                 return NotFound(new { message = "Categoria não encontrada!" });
@@ -48,13 +66,47 @@ namespace Api_Shop.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            return Ok(model);
+            try
+            {
+                db.Entry<Category>(model).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return model;
+            }
+            catch (DbUpdateConcurrencyException db_ex)
+            {
+                return BadRequest(new { message = $"ERRO! Registro atualizado anteriormente, tente novamente depois: {db_ex.Message}" });
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(new { message = $"ERRO! Não foi possível atualizar categoria: {ex.Message}" });
+            }
         }
 
         [HttpDelete]
-        [Route("")]
-        public async Task<ActionResult<Category>> Delete()
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Delete(
+            int id,
+            [FromServices] DataContext db)
         {
+            var category = await db
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound(new { message = "Categoria não encontrada!" });
+            }
+
+            try
+            {
+                db.Categories.Remove(category);
+                await db.SaveChangesAsync();
+                return Ok(new { message = "Categoria removida!" });
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(new { message = $"ERRO! Não foi possível remover a categoria: {ex.Message}" });
+            }
+
             return Ok();
         }
     }
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_Shop.Data;
+using Api_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        #region Todas as maneiras possíveis de listar produtos
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext db)
+        {
+            var products = await db.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext db, int id)
+        {
+            var product = await db.Products
+                        .Include(x => x.Category)
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext db, int id)
+        {
+            var products = await db.Products
+                        .Include(x => x.Category)
+                        .AsNoTracking()
+                        .Where(x => x.CategoryId == id)
+                        .ToListAsync();
+            return products;
+        }
+        #endregion
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post([FromServices] DataContext db, [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(model);
+                await db.SaveChangesAsync();
+                return Ok(model);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
